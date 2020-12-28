1c7824c9778f8ca594ce1f59af61dca8795b8ceb
“yebenxiaozhang <736297001@qq.com>
2020-12-28 15:36:24 +0800

    1

--- XFP/KDY_API/test_case/APP_My_visit_kh.py
+++ XFP/KDY_API/test_case/APP_My_visit_kh.py
@@ -4,7 +4,7 @@
 # @File    : My_visit_casc.py
 
 """我的带看-相关"""
-from XFP.PubilcAPI.flowPath import *
+from PubilcAPI.flowPath import *
 
 """
 无审核-正常流程：·····现状态··················· 流放公海状态
@@ -72,21 +72,141 @@ class TestCase(unittest.TestCase):
         while cls.appApi.appText.get('web_total') != 0:
             cls.webApi.audit(auditStatue=2, auditRemark=' 审核失败')
             cls.webApi.auditList(auditLevel=2)
+        cls.do_request = appApi()
+        cls.appApi = cls.do_request
+        cls.appApi.Login()
+        cls.appApi.GetUserData()
+        cls.request = webApi()
+        cls.webApi = cls.request
+        cls.webApi.Audit_management()
+        cls.flow = flowPath()
+        cls.flowPath = cls.flow
+        cls.appText = GlobalMap()
+        """线索来源"""
+        cls.flowPath.get_label(labelNo='XSLY', labelName='线索来源',
+                               newlabelName='百度小程序')
+        cls.appText.set_map('XSLY', cls.appText.get('labelId'))
+
+        """线索来源_幸福派总部"""
+        cls.flowPath.get_label(labelNo='XSLY', labelName='线索来源',
+                               newlabelName='幸福派总部')
+        cls.appText.set_map('XSLY_admin', cls.appText.get('labelId'))
+        """线索标签"""
+        cls.appApi.GetUserLabelList(userLabelType='线索标签')
+        if cls.appText.get('total') == 0:
+            cls.appApi.AddUserLabel()
+            cls.appApi.GetUserLabelList(userLabelType='线索标签')
+        cls.appText.set_map('XSBQ', cls.appText.get('labelData'))
+        """终止跟进"""
+        cls.flowPath.get_label(labelNo='SZGJYY', labelName='终止跟进原因',
+                               newlabelName='客户已成交')
+        cls.appText.set_map('ZZGJ', cls.appText.get('labelId'))
+        """成交项"""
+        cls.flowPath.get_label(labelNo='CJX', labelName='成交项目',
+                               newlabelName='认购')
+        cls.appText.set_map('CJX', cls.appText.get('labelId'))
+        """出行方式"""
+        cls.flowPath.get_label(labelNo='CXFS', labelName='出行方式',
+                               newlabelName='自驾')
+        cls.appText.set_map('CXFS', cls.appText.get('labelId'))
+        """客户意向等级"""
+        cls.appApi.GetLabelList(labelNo='KHYXDJ')                       # 查询购房意向loanSituation
+        cls.appText.set_map('KHYXDJ', cls.appText.get('labelId'))
+        cls.appApi.GetLabelList(labelNo='ZJZZ')                         # 查询资金资质
+        cls.appText.set_map('ZJZZ', cls.appText.get('labelId'))
+        cls.appApi.GetLabelList(labelNo='GFMD')                         # 查询购房目的
+        cls.appText.set_map('GFMD', cls.appText.get('labelId'))
+        cls.appApi.GetLabelList(labelNo='WYSX')                         # 查询物业属性
+        cls.appText.set_map('WYSX', cls.appText.get('labelId'))
+        cls.appApi.GetLabelList(labelNo='GFZZ')                         # 查询购房资质
+        cls.appText.set_map('GFZZ', cls.appText.get('labelId'))
+        cls.appApi.GetLabelList(labelNo='SFSTF')                        # 查询是否首套
+        cls.appText.set_map('SFSTF', cls.appText.get('labelId'))
+        cls.appApi.GetMatchingArea()                                    # 查询匹配区域
+        cls.appApi.GetMatchingAreaHouse()                               # 匹配楼盘
+        cls.appApi.GetLabelList(labelNo='QTKHXQ')                       # 查询客户需求
+        cls.appText.set_map('QTKHXQ', cls.appText.get('labelId'))
+        cls.appApi.ConsultantList()                                     # 咨询师列表
+        cls.appApi.GetLabelList(labelNo='SQZHGJ', labelName='其他')
+        cls.appText.set_map('ZHGJ', cls.appText.get('labelId'))         # 暂缓跟进
+        cls.flowPath.get_label(labelNo='XXFL', labelName='信息分类',
+                               newlabelName='信息分类一')
+        cls.appText.set_map('XXFL', cls.appText.get('labelId'))         # 信息分类
+        cls.flowPath.get_label(labelNo='DLGS', labelName='代理公司',
+                               newlabelName='代理公司一')
+        cls.appText.set_map('DLGS', cls.appText.get('labelId'))         # 代理公司
+        cls.flowPath.get_label(labelNo='WDFL', labelName='问答分类',
+                               newlabelName='问答分类一')
+        cls.appText.set_map('WDFL', cls.appText.get('labelId'))         # 问答分类
+        cls.webApi.consultant_allocition(isAppoint=1)
+
+        cls.appApi.GetLabelList(labelNo='CFZLX', labelName='成交奖励', saasCode='admin')
+        cls.appText.set_map('CJJL', cls.appText.get('remark'))
+        cls.appApi.GetLabelList(labelNo='CFZLX', labelName='邀约带看', saasCode='admin')
+        cls.appText.set_map('YYDK', cls.appText.get('remark'))
+        cls.webApi.get_group()
+        cls.appApi.GetLabelList(labelNo='CFZLX', labelName='平台上户', saasCode='admin')
+        cls.appText.set_map('PTSH', cls.appText.get('remark'))
+        cls.appApi.GetLabelList(labelNo='XSSPYY', labelName='电话空号', saasCode='admin')
+        cls.appText.set_map('DHWK', cls.appText.get('labelId'))
+        cls.appApi.get_current_month_start_and_end(date=time.strftime("%Y-%m-%d"))
 
     def test_my_visit_01(self):
         """1、创建带看          进行中                   已取消"""
-        self.flowPath.add_visit()
+        self.flowPath.client_list_non_null()
+        self.appApi.ClientTask(taskType='3')
+        if self.appApi.appText.get('total') == 2:
+            self.flowPath.advance_over_visit()
+            if self.appApi.appText.get('code') != 200:
+                self.flowPath.clue_non_null()
+                self.appApi.ClientEntering(callName=self.appApi.RandomText(textArr=surname),
+                                           loanSituation='这个是贷款情况')
+                self.flowPath.client_list_non_null()
+        self.appApi.ClientList()
+        dome = time.strftime("%Y-%m-%d %H:%M:%S")
+        self.flowPath.flowPathText.set_map('time', dome)
+        self.appApi.ClientVisitAdd(projectAId=self.appApi.appText.get('houseId'),
+                                   appointmentTime=dome,
+                                   seeingConsultant=self.appApi.appText.get('consultantId'),
+                                   appointConsultant=self.appApi.appText.get('consultantId'))
         self.flowPath.visit_status(status='无需审核')
+        # 查看新增客户带看后首页的条数
+        self.appApi.GetUserAgenda()
+        dome = self.appText.get('total')
+        tomorrow1 = (date.today() + timedelta(days=7)).strftime("%Y-%m-%d")
+        self.appApi.Task_Visit_List(endTime=tomorrow1 + ' 23:59:59', visiteStatus=0)
+        dome1 = self.appText.get('total')
+        """存在带看以及客户跟进的客户进行流放公海"""
         self.appApi.client_exile_sea()
         self.flowPath.visit_status(status='已取消')
 
+        # 验证流放公海的前后进行对比
+        self.appApi.GetUserAgenda()
+        self.assertEqual(dome - 2, self.appText.get('total'))
+        self.appApi.Task_Visit_List(endTime=tomorrow1 + ' 23:59:59', visiteStatus=0)
+        self.assertEqual(dome1, self.appText.get('total'))
+
     def test_my_visit_02(self):
         """2、完成带看          已完成                   已完成"""
+        """1- 客户创建带看计划  首页带看任务会增加"""
         self.flowPath.add_visit()
+        # 验证创建带看后是否有  带看任务新增
+        self.appApi.GetUserAgenda()
+        dome = self.appText.get('total')
+        self.appApi.ClientList()
         self.flowPath.accomplish_visit()
         self.flowPath.visit_status(status='已完成')
-        self.appApi.client_exile_sea()
-        self.flowPath.visit_status(status='已完成')
+        self.appApi.GetUserAgenda()
+        self.assertEqual(dome - 1, self.appText.get('total'))
+        """录入带看在7天后"""
+        tomorrow = (date.today() + timedelta(days=9)).strftime("%Y-%m-%d %H:%M:%S")
+        self.appApi.ClientList()
+        self.appApi.ClientVisitAdd(projectAId=self.appApi.appText.get('houseId'),
+                                   appointmentTime=tomorrow,
+                                   seeingConsultant=self.appApi.appText.get('consultantId'),
+                                   appointConsultant=self.appApi.appText.get('consultantId'))
+        self.appApi.GetUserAgenda()
+        self.assertEqual(dome - 1, self.appText.get('total'))
 
     def test_my_visit_03(self):
         """3、取消带看      已取消                   已取消"""
@@ -111,13 +231,44 @@ class TestCase(unittest.TestCase):
     def test_my_visit_06(self):
         """3、创建带看-审核失败                 已驳回             已驳回"""
         self.webApi.Audit_management(customerVisit=True, customerVisitLevel=1)  # 修改配置审核
-        self.flowPath.add_visit()
+        self.flowPath.client_list_non_null()
+        self.appApi.ClientTask(taskType='3')
+        if self.appApi.appText.get('total') == 2:
+            self.flowPath.advance_over_visit()
+            if self.appApi.appText.get('code') != 200:
+                self.flowPath.clue_non_null()
+                self.appApi.ClientEntering(callName=self.appApi.RandomText(textArr=surname),
+                                           loanSituation='这个是贷款情况')
+                self.flowPath.client_list_non_null()
+        """首页近期带看列表以及待跟进两个接口是否有新增"""
+        self.appApi.GetUserAgenda()
+        dome = self.appText.get('total')
+        tomorrow1 = (date.today() + timedelta(days=7)).strftime("%Y-%m-%d")
+        self.appApi.Task_Visit_List(endTime=tomorrow1 + ' 23:59:59', visiteStatus=0)
+        dome1 = self.appText.get('total')
+        self.appApi.ClientList()
+        self.appApi.ClientVisitAdd(projectAId=self.appApi.appText.get('houseId'),
+                                   appointmentTime=time.strftime("%Y-%m-%d %H:%M:%S"),
+                                   seeingConsultant=self.appApi.appText.get('consultantId'),
+                                   appointConsultant=self.appApi.appText.get('consultantId'))
+        """录入带看 审核中的 是否有新增"""
+        self.appApi.GetUserAgenda()
+        self.assertEqual(dome + 1, self.appText.get('total'))
+        self.appApi.Task_Visit_List(endTime=tomorrow1 + ' 23:59:59', visiteStatus=0)
+        self.assertEqual(dome1 + 1, self.appText.get('total'))
+
         self.webApi.auditList(phoneNum=self.appText.get('cluePhone'))
         self.webApi.audit(auditStatue=2, auditRemark=time.strftime("%Y-%m-%d %H:%M:%S") + ' 审核失败')
 
         self.flowPath.visit_status(status='已驳回')
         self.appApi.client_exile_sea()
         self.flowPath.visit_status(status='已取消')
+
+        """审核失败后进行查看是否有新增"""
+        self.appApi.GetUserAgenda()
+        self.assertEqual(dome - 1, self.appText.get('total'))
+        self.appApi.Task_Visit_List(endTime=tomorrow1 + ' 23:59:59', visiteStatus=0)
+        self.assertEqual(dome1 + 1, self.appText.get('total'))
 
     def test_my_visit_07(self):
         """4、审核成功-完成带看                 已完成             已完成"""
@@ -249,6 +400,12 @@ class TestCase(unittest.TestCase):
 
     def test_my_visit_19(self):
         """同一个客户只能存在一个带看 一个带看代办"""
+        self.appApi.Login(userName=XfpUser1)
+        self.appApi.GetUserData()
+        self.appApi.GetUserAgenda()
+        zxs_04_db = self.appText.get('total')
+        self.appApi.Login()
+        self.appApi.GetUserData()
         self.webApi.Audit_management()
         self.flowPath.add_visit()
         self.appApi.ClientVisitAdd(projectAId=self.appApi.appText.get('houseId'),
@@ -256,6 +413,18 @@ class TestCase(unittest.TestCase):
                                    appointConsultant=self.appApi.appText.get('consultantId'))
         self.assertEqual(301, self.appText.get('code'))
         self.assertEqual('该客户存在未完成带看', self.appApi.appText.get('data'))
+        self.appApi.ClientFollowList()
+        self.appApi.ClueFollowSave(followType='客户', taskEndTime=time.strftime("%Y-%m-%d") + ' 22:00:00')
+        self.appApi.GetUserAgenda()
+        zxs_01_db = self.appText.get('total')
+        """客户转移 有两个待办（客户跟进 + 客户带看）"""
+        self.appApi.client_change()
+        self.appApi.GetUserAgenda()
+        self.assertEqual(zxs_01_db - 2, self.appText.get('total'))
+        self.appApi.Login(userName=XfpUser1)
+        self.appApi.GetUserData()
+        self.appApi.GetUserAgenda()
+        self.assertEqual(zxs_04_db + 2, self.appText.get('total'))
         self.flowPath.accomplish_visit()
 
 
